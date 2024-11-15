4de761e8d1f33c0f4eac31103af6be5a5e60db05
saniyafarzindeveloper <developerfarzin@gmail.com>
2024-11-15 23:02:15 +0530

    user schema

--- src/model/User.ts
+++ src/model/User.ts
@@ -20,3 +20,29 @@ const MessageSchema: Schema<Message> = new Schema({
     default: Date.now,
   },
 });
+
+
+export interface User extends Document {
+ username: string;
+ email: string;
+ password: string;
+ verifyCode: string;
+ verifyCodeExpiry: Date;
+ isAcceptingMessage: boolean;
+ message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true
+  },
+  email:{
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, 'Please use a valid e-mail']
+  }
+});
