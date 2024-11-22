4a365b2d85b4c418555c698b0e17b8178bc2db35
saniyafarzindeveloper <developerfarzin@gmail.com>
2024-11-22 15:48:24 +0530

    completed ZOD

--- src/schemas/acceptMessageSchema.ts
+++ src/schemas/acceptMessageSchema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const acceptMessageSchema = z.object({
   acceptMessages: z.boolean (),
- 
+  
 });
--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const messageSchema = z.object({
-  content: z.string ().min(1, {message: 'Content must be of at least 10 characters'})
- 
+  content: z
+    .string()
+    .min(10, { message: "Content must be of at least 10 characters" })
+    .max(300, { message: "Message exceeding limit" }),
 });
